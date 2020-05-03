d65f5614662324950cf8a95522fa74db5064324f
magicmmp <2233973404@qq.com>
2020-05-03 11:50:37 +0800

    auto git push  CodeBlocks Project: 2020年05月 3日 11:50:37

--- cstdio/main.cpp
+++ cstdio/main.cpp
@@ -3,6 +3,7 @@
 #include<iomanip>
 #include <errno.h>
 #include <string.h>
+#include <math.h>
 using namespace std;
 /*
 char buff[48];
@@ -26,6 +27,12 @@ int main()
     char *str="abcd";
     char bf[]="abcd";
     printf("%d,%d,%d\n",sizeof(str),sizeof(bf),sizeof("abcd"));
+    double y;
+    for(tmp=40;tmp<70;tmp+=5)
+    {
+
+        printf("Õñ·ù1=%d Õñ·ù2 = %d\n",tmp,sqrt(tmp));
+    }
 
 
     return 0;
